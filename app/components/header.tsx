48e0af7fb0bbdf3fd8b4062c83fef404ffaad0e7
Alex Marmolejo <42818181+prosperitty@users.noreply.github.com>
2024-12-15 03:02:08 -0500

    changed logo to arcade.io

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Header() {
   return (
     <header className='flex shadow-inner shadow-purple-300'>
-      <Image
+      <h1 className='hidden md:block text-6xl p-4 text-red-300 orange-shadow leading-none'>
+        ARCADE.io
+      </h1>
+      <h1 className='md:hidden flex justify-center items-center text-6xl px-4 py-4 text-red-300 orange-shadow'>
+        A
+      </h1>
+      {/* <Image
         src='/assets/Miniclip_Logo_Small.svg'
         width={50}
         height={50}
@@ -16,7 +22,7 @@ export default function Header() {
         height={400}
         alt='Miniclip logo'
         className='hidden md:block m-4'
-      />
+      /> */}
 
       <div className='flex-grow border-l-2 border-purple-500'>
         <div className='relative h-full px-4 py-2'>
@@ -27,7 +33,7 @@ export default function Header() {
             unoptimized
           />
           <h2 className='absolute m-0 text-teal-300 blue-shadow text-xl'>
-            Welcome to Miniclip!
+            Welcome to Arcade.io!
           </h2>
         </div>
       </div>
